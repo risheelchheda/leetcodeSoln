8dc79dd7c1ac300c5b1819d3ec4858b8572d4eed
Risheel Chheda <98162095+risheelchheda@users.noreply.github.com>
2023-03-04 18:33:11 +0530

    Create 328.py

---
+++ 328.py
@@ -0,0 +1,13 @@
+if head==None:
+            return head
+        odd=head
+        even=head.next
+        evenHead=even
+        while even and even.next:
+            odd.next=even.next
+            odd=odd.next
+
+            even.next=odd.next
+            even=even.next
+        odd.next=evenHead
+        return head
