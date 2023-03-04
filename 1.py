74f2893d90648af3a187ba91a04322aeb6ca8235
Risheel Chheda <98162095+risheelchheda@users.noreply.github.com>
2023-03-04 10:37:17 +0530

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,7 @@
+hashmap = {}
+        for i in range(len(nums)):
+            hashmap[nums[i]] = i
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap and hashmap[complement] != i:
+                return [i, hashmap[complement]]
