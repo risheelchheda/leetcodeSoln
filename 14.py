984361a520dbf38bbc979d8f970b9ed42778ffef
Risheel Chheda <98162095+risheelchheda@users.noreply.github.com>
2023-03-04 10:42:40 +0530

    Create 14.py

---
+++ 14.py
@@ -0,0 +1,10 @@
+if len(strs) == 0:
+            return '' 
+        res = ''
+        strs = sorted(strs)
+        for i in strs[0]:
+            if strs[-1].startswith(res+i):
+                res += i
+            else:
+                break
+        return res
