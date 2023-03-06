1e07275bad224420b64a1d4efaca97fca5024e10
Risheel Chheda <98162095+risheelchheda@users.noreply.github.com>
2023-03-06 19:49:31 +0530

    Update 105.py

--- 105.py
+++ 105.py
@@ -1,13 +1,14 @@
 in_map={val:i for i,val in enumerate(inorder)}
-        def build_tree(pre_start,pre_end,in_start,in_end):
-            if pre_start>pre_end:
+        def build(start,end):
+            if start>end:
                 return None
-            val=preorder[pre_start]
+            
+            val=preorder.pop(0)
             root=TreeNode(val)
             ind=in_map[val]
-            left_size=ind-in_start
-            root.left=build_tree(pre_start+1,pre_start+left_size,in_start,ind-1)
-            root.right=build_tree(pre_start+left_size+1,pre_end,ind+1,in_end)
+
+            root.left=build(start,ind-1)
+            root.right=build(ind+1,end)
             return root
         
-        return build_tree(0,len(preorder)-1,0,len(inorder)-1)
+        return build(0,len(inorder)-1)
