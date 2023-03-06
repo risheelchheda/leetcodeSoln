dc9b723e44bfbc788c4d5b08672ac0b0e1b3c4f3
Risheel Chheda <98162095+risheelchheda@users.noreply.github.com>
2023-03-06 17:08:17 +0530

    Create 100.py

---
+++ 100.py
@@ -0,0 +1,8 @@
+if not p and not q:
+            return True
+        elif not p or not q:
+            return False
+        elif p.val!=q.val:
+            return False
+        else:
+            return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
