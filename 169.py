2ade7e37ed803bd34809b390f2dcf9b5b52d9d94
Risheel Chheda <98162095+risheelchheda@users.noreply.github.com>
2023-03-23 16:47:36 +0530

    Create 169.py

---
+++ 169.py
@@ -0,0 +1,7 @@
+count=0
+        candidate=None
+        for i in nums:
+            if count==0:
+                candidate=i
+            count+=(1 if i==candidate else -1)
+        return candidate
