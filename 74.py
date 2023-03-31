6047f9fed4e357f4076bd6141c97b6c82f5d8b11
Risheel Chheda <98162095+risheelchheda@users.noreply.github.com>
2023-03-31 17:25:01 +0530

    Update 74.py

--- 74.py
+++ 74.py
@@ -1,6 +1,15 @@
-if not matrix or not matrix[0]:
-            return False
+row=0
         for i in range(len(matrix)):
-            if target in matrix[i]:
+            if target>=matrix[i][0]:
+                row=i
+        l=0
+        r=len(matrix[row])-1
+        while r>=l:
+            mid=(l+r)//2
+            if matrix[row][mid]==target:
                 return True
+            elif matrix[row][mid]>target:
+                r=mid-1
+            else:
+                l=mid+1
         return False
