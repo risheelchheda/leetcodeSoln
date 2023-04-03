a52d6e77901266d6ea0d074c5f19c31954a74f40
Risheel Chheda <98162095+risheelchheda@users.noreply.github.com>
2023-04-03 12:46:13 +0530

    Create 215.py

---
+++ 215.py
@@ -0,0 +1,8 @@
+minHeap = []
+
+        for num in nums:
+            heapq.heappush(minHeap, num)
+            if len(minHeap) > k:
+                heapq.heappop(minHeap)
+
+        return minHeap[0]
