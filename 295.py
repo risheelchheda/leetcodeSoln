ede91c577890ee0a3256f4e2db84c56aa9c2c959
Risheel Chheda <98162095+risheelchheda@users.noreply.github.com>
2023-04-04 11:01:04 +0530

    Create 295.py

---
+++ 295.py
@@ -0,0 +1,16 @@
+def __init__(self):
+        self.small = []
+        self.large = []
+        
+
+    def addNum(self, num: int) -> None:
+        if len(self.small) == len(self.large):
+            heapq.heappush(self.large, -heapq.heappushpop(self.small, -num))
+        else:
+            heapq.heappush(self.small, -heapq.heappushpop(self.large, num))
+
+    def findMedian(self) -> float:
+        if len(self.small) == len(self.large):
+            return (self.large[0] - self.small[0]) / 2.0
+        else:
+            return float(self.large[0])
