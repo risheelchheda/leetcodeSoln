92af85d8a049ea8474a7749100ec11f89c452799
Risheel Chheda <98162095+risheelchheda@users.noreply.github.com>
2023-04-05 16:35:17 +0530

    Update 242.py

--- 242.py
+++ 242.py
@@ -1,5 +1,15 @@
-s=sorted(s)
-        t=sorted(t)
-        if s==t:
-            return True
-        return False
+if len(s) != len(t):
+            return False
+    
+        # Create two dictionaries to store the frequency of each character in s and t
+        s_freq = {}
+        t_freq = {}
+        
+        # Loop through each character in s and t
+        for i in range(len(s)):
+            # Increment the frequency of the current character in the corresponding dictionary
+            s_freq[s[i]] = s_freq.get(s[i], 0) + 1
+            t_freq[t[i]] = t_freq.get(t[i], 0) + 1
+            
+        # Compare the two dictionaries
+        return s_freq == t_freq
