1fcea07ff4c97a393ac563c06e348e50b7581567
Risheel Chheda <98162095+risheelchheda@users.noreply.github.com>
2023-04-19 14:14:09 +0530

    Update 746.py

--- 746.py
+++ 746.py
@@ -11,3 +11,26 @@ def solve(n,dp,cost):
         cost.append(0)
         dp=[-1 for i in range(n+1)]
         return solve(n,dp,cost)
+
+#Bottom up approach
+n=len(cost)
+        cost.append(0)
+        dp=[-1 for i in range(n+1)]
+        dp[0]=cost[0]
+        dp[1]=cost[1]
+        for i in range(2,n+1):
+            dp[i]=min(dp[i-1],dp[i-2])+cost[i]
+        return dp[n]
+
+#Optimised Solution
+n=len(cost)
+        cost.append(0)
+        a=cost[0]
+        b=cost[1]
+        if n==0:
+            return a
+        for i in range(2,n+1):
+            curr=min(a,b)+cost[i]
+            a=b
+            b=curr
+        return b
