89942dec7998b13dc356cc7f3a1da8794b572c5f
Risheel Chheda <98162095+risheelchheda@users.noreply.github.com>
2023-04-23 12:37:29 +0530

    Create 96.py

---
+++ 96.py
@@ -0,0 +1,14 @@
+#TOP BOTTOM APPROACH
+def solve(n,dp):
+            if n<=1:
+                return 1
+            if dp[n]!=-1:
+                return dp[n]
+            ans=0
+            for i in range(1,n+1):
+                ans+= solve(i-1,dp)*solve(n-i,dp)
+            dp[n]=ans
+            return dp[n]
+        
+        dp=[-1 for i in range(n+1)]
+        return solve(n,dp
